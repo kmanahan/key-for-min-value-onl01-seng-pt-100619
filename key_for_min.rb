f8cbc1a6f3836df5306ad996f2a06d588f4baa38
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-10-29 17:34:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,23 +3,26 @@
 
 def key_for_min_value(hash)
   min = nil 
+  min_value = nil
   hash.collect do |key, value|
-    if  min == nil || value < min
-      min 
+    if  min_value == nil || value < min_value
+      min_value = value 
+      min = key
     else 
       puts key[:value]
   end
 end
+min
 end
 
-def key_for_min_value(name_hash)
-  lowest_key = nil 
-  lowest_value = nil 
-  name_hash.each do |k, v|
-    if lowest_value == nil || v < lowest_value
-      lowest_value = v 
-      lowest_key = k 
-    end 
-  end 
-  lowest_key
-end
+# def key_for_min_value(name_hash)
+#   lowest_key = nil 
+#   lowest_value = nil 
+#   name_hash.each do |k, v|
+#     if lowest_value == nil || v < lowest_value
+#       lowest_value = v 
+#       lowest_key = k 
+#     end 
+#   end 
+#   lowest_key
+# end
